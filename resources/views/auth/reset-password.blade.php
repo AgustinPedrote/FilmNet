be9a6e2c15c6a575c9f981b424ab61575417ad2e
agucadiz <agustin.pedrote@iesdonana.org>
2023-11-24 19:07:40 +0100

    Recuperación de contraseña implementado

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,27 +1,32 @@
 <x-guest-layout>
-    <form method="POST" action="{{ route('password.store') }}">
+    <form class="bg-blue-500 py-8 px-6 rounded-lg w-full max-w-md mx-auto" method="POST" action="{{ route('password.store') }}">
         @csrf
+
+        <!-- Imagen -->
+        <div class="flex items-center justify-center mb-6">
+            <img src="{{ asset('img/logo3.png') }}" alt="Logo" class="w-3/4 object-contain">
+        </div>
 
         <!-- Password Reset Token -->
         <input type="hidden" name="token" value="{{ $request->route('token') }}">
 
         <!-- Email Address -->
         <div>
-            <x-input-label for="email" :value="__('Email')" />
+            <x-input-label class="text-white" for="email" :value="__('Email')" />
             <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email', $request->email)" required autofocus autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
         <!-- Password -->
         <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+            <x-input-label class="text-white" for="password" :value="__('Password')" />
             <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
         <!-- Confirm Password -->
         <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+            <x-input-label class="text-white" for="password_confirmation" :value="__('Confirm Password')" />
 
             <x-text-input id="password_confirmation" class="block mt-1 w-full"
                                 type="password"
@@ -32,7 +37,7 @@
 
         <div class="flex items-center justify-end mt-4">
             <x-primary-button>
-                {{ __('Reset Password') }}
+                {{ __('Restablecer contraseña') }}
             </x-primary-button>
         </div>
     </form>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -32,7 +32,7 @@
                 </div>
 
                 {{-- Buscador --}}
-                <div class="w-60 items-center hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                <div class="w-72 items-center hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <input type="text" id="search-navbar"
                         class="h-10 block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500
                         focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
